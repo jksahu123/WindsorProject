6d10b2ef33afd76e140c9742375e0913de9c5527
AjeetSahu <ajeetkumar.sahu@sjsu.edu>
2016-08-14 12:16:13 -0400

    frontend and controllers - home and post form

---
+++ controller/formController.js
@@ -0,0 +1,52 @@
+var app = angular.module('postApp',[]);
+
+         app.directive('fileModel', ['$parse', function ($parse) {
+            return {
+               restrict: 'A',
+               link: function(scope, element, attrs) {
+                  var model = $parse(attrs.fileModel);
+                  var modelSetter = model.assign;
+                  
+                  element.bind('change', function(){
+                     scope.$apply(function(){
+                        modelSetter(scope, element[0].files[0]);
+                     });
+                  });
+               }
+            };
+         }]);
+
+         app.service('fileUpload', ['$http', function ($http) {
+            this.uploadFileToUrl = function(file, uploadUrl){
+               var fd = new FormData();
+               fd.append('file', file);
+            
+               $http.post(uploadUrl, fd, {
+                  transformRequest: angular.identity,
+                  headers: {'Content-Type': undefined}
+               })
+            
+               .success(function(){
+               	console.log("upload successful");
+               })
+            
+               .error(function(){
+               	console.log("upload errorfull");
+               });
+            }
+         }]);
+
+app.controller('formCtrl',['$scope', 'fileUpload', function($scope, fileUpload){
+            $scope.uploadFile = function(){
+               var file = $scope.myFile;
+               
+               console.log('file is ' );
+               console.dir(file);
+               
+               var uploadUrl = "//C://Windsor Proj//images";
+               fileUpload.uploadFileToUrl(file, uploadUrl);
+            };
+	$scope.submit = function(){
+		console.log($scope.post);
+	}
+}]);
---
+++ controller/homeController.js
@@ -0,0 +1,79 @@
+var app = angular.module('homeApp',[]);
+app.controller('homeCtrl', function($scope){
+	var arr = [
+		{	
+			"heading": "I want to sell me Fan and kursi maze",
+			"name": "bintu",
+			"price": 300,
+			"condition": "Used",
+			"address": "11 E",
+			"emailId": "aks.comet@gmail.com",
+			"phone": "4547944756",
+			"detail": "Posting on behalf of two female friends. They are looking for a private room to be shared among themselves in the range 1000-1100$. Move-in date: 01 September. Area: San Jose/Sunnyvale/MTV preferred."
+		},
+		{	
+			"heading": "I want to donate some kitchen items",
+			"name": "jyoti",
+			"price": 300,
+			"condition": "Used",
+			"address": "1 C",
+			"emailId": "aks.comet@gmail.com",
+			"phone": "4547944756",
+			"detail": "ISO-SJSU would like to thank all the alumni members who took time out and participated in that friendly game."
+		},
+		{
+			"heading": "Free free Free !!!",
+			"name": "jitendra",
+			"price": 500,
+			"condition": "3 months old",
+			"address": "12 B",
+			"emailId": "aks.comet@gmail.com",
+			"phone": "4547944756",
+			"detail": "Display the latest 10 posts in a static HTML page (8 posts) ... Here is the page renamed with a PHP extention, which I'm assuming must be done, and the errors my code is displaying: ... Ah - I see the read more in your code - many thanks!"
+		},
+				{
+			"heading": "Free free Free !!!",
+			"name": "jitendra",
+			"price": 500,
+			"condition": "3 months old",
+			"address": "12 B",
+			"emailId": "aks.comet@gmail.com",
+			"phone": "4547944756",
+			"detail": "Display the latest 10 posts in a static HTML page (8 posts) ... Here is the page renamed with a PHP extention, which I'm assuming must be done, and the errors my code is displaying: ... Ah - I see the read more in your code - many thanks!"
+		},
+				{
+			"heading": "Free free Free !!!",
+			"name": "jitendra",
+			"price": 500,
+			"condition": "3 months old",
+			"address": "12 B",
+			"emailId": "aks.comet@gmail.com",
+			"phone": "4547944756",
+			"detail": "Display the latest 10 posts in a static HTML page (8 posts) ... Here is the page renamed with a PHP extention, which I'm assuming must be done, and the errors my code is displaying: ... Ah - I see the read more in your code - many thanks!"
+		}
+	];
+	$scope.arr = arr;
+	//$scope.price = "500 rupaya";
+/*	var obj = [
+		{
+			"name": "ajeet",
+			"phone": "4086279583"
+			"email": "aks.comet@gmail.com",
+			"price": 50	
+		},
+			{
+			"name": "ramu",
+			"phone": "4086279583"
+			"email": "ramu.comet@gmail.com",
+			"price": 100
+		},
+			{
+			"name": "shyamu",
+			"phone": "4086279583"
+			"email": "shyamu.comet@gmail.com",
+			"price": 200
+		}
+	];
+	
+	$scope.obj = obj;
+*/});
